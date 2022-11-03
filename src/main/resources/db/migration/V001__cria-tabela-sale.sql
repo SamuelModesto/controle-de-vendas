a001ed212e4d19014dd1c1e2e6d4f393274f2da0
Samuel Modesto <samuelmodestoes@gmail.com>
2022-11-02 23:01:13 -0300

    adicionando flyway no projeto

---
+++ src/main/resources/db/migration/V001__cria-tabela-sale.sql
@@ -0,0 +1,7 @@
+create table tb_sales
+(
+    id      bigint  not null auto_increment,
+    visited bigint  not null,
+    deals   integer not null,
+    primary key (id)
+);
---
+++ src/main/resources/db/migration/V002__nova-coluna-amout.sql
@@ -0,0 +1,1 @@
+alter table tb_sales add amount double;
